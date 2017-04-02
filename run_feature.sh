74e5b30ff72fb55532257be5eafa6a012bdcbbe8
qianqian <qianqian@moseeker.com>
2017-04-02 21:37:21 +0800

    init

---
+++ run_feature.sh
@@ -0,0 +1,7 @@
+python generate_stem.py
+python generate_interaction.py
+python generate_tfidf.py
+python generate_len.py
+python generate_distance.py
+python generate_svm_format_tfidf.py
+pypy sgd_fm.py
